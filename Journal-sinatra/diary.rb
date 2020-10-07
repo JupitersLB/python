7e1e21852298820f2156a189a5f1fb277fda9590
Liam Baker <liam.baker2310@gmail.com>
2020-10-07 12:04:52 +0900

    Revert "first commit"
    This reverts commit a32e9523b8f2b5404f629aac79fc59468baa06a5.

--- Journal-sinatra/diary.rb
+++
@@ -1,8 +0,0 @@
-# Creates a diary with date and entry.
-class Diary
-  attr_accessor :date_input, :entry
-  def initialize(date_input, entry)
-    @date_input = date_input
-    @entry = entry
-  end
-end
--- Journal-sinatra/users.rb
+++
@@ -1,8 +0,0 @@
-# Creates a student with name and csv_file_path.
-class User
-  attr_reader :name, :csv_file_path
-  def initialize(name)
-    @name = name
-    @csv_file_path = "#{@name}_entries.csv"
-  end
-end
